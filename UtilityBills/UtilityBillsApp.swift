66332e3e7cde2307cad183058039784ef6646a76
Sergey Chelak <sergey.chelak@gmail.com>
2024-08-18 01:07:32 +0300

    Initial Commit

---
+++ UtilityBills/UtilityBillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtilityBillsApp.swift
+//  UtilityBills
+//
+//  Created by Sergey on 18.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct UtilityBillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
